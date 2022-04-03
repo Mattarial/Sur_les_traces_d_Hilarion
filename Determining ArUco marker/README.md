281e4173ddd808f8182287737939af2b40049b8e
dylan_jacquot <Dylan.Jacquot.Etu@univ-lemans.fr>
2022-04-03 02:22:28 +0200

    V1.0.0 Réorganisation complète du dossier projet

--- Determining ArUco marker/README.md
+++
@@ -1,2 +0,0 @@
-# USAGE
-> python guess_aruco_type.py --image images/example_01.png
--- aruco_4x4_100_utils.py
+++ aruco_4x4_100_utils.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python
-
-# import the necessary packages
-import math
-
-import numpy as np
-from imutils.video import VideoStream
 import argparse
-import imutils
-import time
-import cv2
+import math
 import sys
-import os
-import glob
+
+import cv2
+import numpy as np
+
 
 def calcCenter(p1,p2):
 	tlx, tlY = p1
@@ -63,7 +56,7 @@ def drawRectangleOnAruco(frame,topRight,topLeft,bottomRight,bottomLeft,color):
 	cv2.line(frame, bottomRight, bottomLeft, color, 2)
 	cv2.line(frame, bottomLeft, topLeft, color, 2)
 
-def getAngleFromCorner(markerCorner,id,dict):
+def getAngleFromCorner(markerCorner,markerID,dict):
 	topRight, topLeft, bottomRight, bottomLeft = getTops(markerCorner)
 	diag = np.array(bottomRight) - np.array(topLeft)
 	finalAngle, _ = math.atan2(diag[0], diag[1]) * 57, 2958
@@ -72,7 +65,7 @@ def getAngleFromCorner(markerCorner,id,dict):
 
 	return dict
 
-def displayAngleOnAruco(frame,angleDict,topLeft):
+def displayAngleOnAruco(frame,markerID,angleDict,topLeft):
 	cv2.putText(frame, "Angle : " + str(int(angleDict[markerID])),
 				(topLeft[0], topLeft[1] - 55),
 				cv2.FONT_HERSHEY_SIMPLEX,
@@ -92,7 +85,7 @@ def drawId(frame,markerID,topLeft):
 				cv2.FONT_HERSHEY_SIMPLEX,
 				0.5, (0, 255, 0), 2)
 
-def getDirection(markerID,lastPoint,dicoDist):
+def getDirection(markerID,lastPoint,cX,cY,dicoDist):
 	if markerID in lastPoint:
 		lpX, lpY = lastPoint.get(markerID)
 	else:
@@ -123,7 +116,7 @@ def getPoleCenters(topRight,topLeft,bottomRight,bottomLeft):
 
 	return pole_centers
 
-def getAllPoleColors(markerID,pole_centers,cX,cY,poleColorsDict):
+def getAllPoleColors(frame,markerID,pole_centers,cX,cY,poleColorsDict):
 	coeff = 1.73
 	colorsByPole = {}
 	i = 0
@@ -178,80 +171,3 @@ def displayPoleColors(markerID, frame, dir,cX,cY ,poleColorsDict,pole_centers):
 					(int(new_center[0]), int(new_center[1])),
 					cv2.FONT_HERSHEY_SIMPLEX,
 					0.5, (int(pixels[0]), int(pixels[1]), int(pixels[2])), 2)
-
-ap,args,arucoDict,arucoParams,ARUCO_DICT = initCvFor4x4()
-
-lastPoint = {}
-angleDict = {}
-dicoDist = {}
-allCenters = {}
-poleColorsDict = {}
-
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
-
-# src is useful to modify the video input
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
-firstTime = True
-
-# loop over the frames from the video stream
-while True:
-	time.sleep(0.08)
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 600 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=1000)
-
-	# detect ArUco markers in the input frame
-	(corners, ids, rejected) = cv2.aruco.detectMarkers(frame,
-		arucoDict, parameters=arucoParams)
-
-	# verify *at least* one ArUco marker was detected
-	if len(corners) > 0:
-		# flatten the ArUco IDs list
-		ids = ids.flatten()
-
-		# loop over the detected ArUCo corners
-		for (markerCorner, markerID) in zip(corners, ids):
-
-			topRight,topLeft,bottomRight,bottomLeft = getTops(markerCorner)
-
-			# draw the bounding box of the ArUCo detection
-			drawRectangleOnAruco(frame,topRight,topLeft,bottomRight,bottomLeft,(0, 255, 0))
-
-			angleDict = getAngleFromCorner(markerCorner,markerID,angleDict)
-
-			displayAngleOnAruco(frame,angleDict,topLeft)
-
-			allCenters = computeCenters(markerID,topLeft,bottomRight,allCenters)
-
-			cX,cY = allCenters[markerID]
-
-			cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
-
-			drawId(frame,markerID,topLeft)
-
-			dicoDist = getDirection(markerID,lastPoint,dicoDist)
-
-			drawDirection(frame,markerID,cX,cY,dicoDist,(255, 0, 0))
-
-			lastPoint[markerID] = (cX,cY)
-
-			pole_centers = getPoleCenters(topRight,topLeft,bottomRight,bottomLeft)
-
-			poleColorsDict = getAllPoleColors(markerID,pole_centers,cX,cY,poleColorsDict)
-
-			displayPoleColors(markerID, frame, dir, cX, cY, poleColorsDict,pole_centers)
-
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
